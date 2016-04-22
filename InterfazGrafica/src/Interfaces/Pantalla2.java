9e00ab837a4f0079f1680f09a0e64e00c19ed341
daniel.garcimartin <daniel.garcimartin@WKSALMPBB442.u-tad.es>
2016-04-22 14:37:37 +0200

    nuevo commit

--- InterfazGrafica/src/Interfaces/Pantalla2.java
+++ InterfazGrafica/src/Interfaces/Pantalla2.java
@@ -23,6 +23,16 @@ import java.awt.event.ContainerEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JPasswordField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SpinnerDateModel;
+import java.util.Date;
+import java.util.Calendar;
+import javax.swing.SpinnerListModel;
+import javax.swing.JList;
 
 public class Pantalla2 extends JFrame {
 
@@ -34,6 +44,12 @@ public class Pantalla2 extends JFrame {
 	private JComboBox comboBox;
 	private JCheckBox chckbxPregunta1 ;
 	private JCheckBox chckbxPregunta2 ;
+	private JRadioButton rdbtnHombre ;
+	private JRadioButton rdbtnMujer ;
+	private  ButtonGroup buttonGroup; 
+	private JButton btnLimpiar;
+	private JPasswordField passwordField;
+	
 
 	/**
 	 * Launch the application.
@@ -56,7 +72,7 @@ public class Pantalla2 extends JFrame {
 	 */
 	public Pantalla2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 614);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -74,21 +90,21 @@ public class Pantalla2 extends JFrame {
 				
 //				comboBox.addItem(txtNombre.getText()+"\n");
 				
-				if(chckbxPregunta1.isSelected())
+				if(chckbxPregunta1.isSelected()){
+					
 					System.out.println("Pregunta 1 seleccionada");
-				else
+				}
+				else{
 					System.out.println("Pregunta 1 no seleccionada");
+				}
 				
 				
-				if(chckbxPregunta2.isSelected())
+				if(chckbxPregunta2.isSelected()){
 					System.out.println("Pregunta 2 seleccionada");
-				else
+				}
+				else{
 					System.out.println("Pregunta 2 no seleccionada");
-				
-				
-				
-				
-				
+				}
 				
 				
 			}
@@ -107,6 +123,65 @@ public class Pantalla2 extends JFrame {
 		chckbxPregunta1 = new JCheckBox("Pregunta1");
 		
 		chckbxPregunta2 = new JCheckBox("Pregunta2");
+		buttonGroup = new ButtonGroup();
+		
+		rdbtnHombre = new JRadioButton("Hombre");
+		buttonGroup.add(rdbtnHombre);
+		
+		rdbtnMujer = new JRadioButton("Mujer");
+		buttonGroup.add(rdbtnMujer);
+		
+		
+		
+		JButton btnOk_1 = new JButton("Ok");
+		btnOk_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnMujer.isSelected()){
+					System.out.println("Es mujer");
+					
+				}else if(rdbtnHombre.isSelected()){
+					System.out.println("Es hombre");
+				}
+				buttonGroup.clearSelection();
+				if(rdbtnMujer.isEnabled()&& rdbtnHombre.isEnabled()){
+					rdbtnMujer.setEnabled(false);
+					rdbtnHombre.setEnabled(false);
+				}else{
+					rdbtnMujer.setEnabled(true);
+					rdbtnHombre.setEnabled(true);
+				}
+				
+				
+			}
+		});
+		
+		btnLimpiar = new JButton("Limpiar");
+		btnLimpiar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chckbxPregunta1.isSelected()){
+					rdbtnMujer.setVisible(!rdbtnMujer.isVisible());
+					rdbtnHombre.setVisible(!rdbtnHombre.isVisible());
+				
+					System.out.println("Pregunta 1 seleccionada");
+				}
+				else{
+					System.out.println("Pregunta 1 no seleccionada");
+				}
+				
+				
+				if(chckbxPregunta2.isSelected()){
+					rdbtnMujer.setVisible(!rdbtnMujer.isVisible());
+					rdbtnHombre.setVisible(!rdbtnHombre.isVisible());
+					System.out.println("Pregunta 2 seleccionada");
+				}
+				else{
+					System.out.println("Pregunta 2 no seleccionada");
+				}
+			}
+		});
+		
+		passwordField = new JPasswordField();
+		
 		
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
@@ -114,49 +189,71 @@ public class Pantalla2 extends JFrame {
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblNombre)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(287, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(379, Short.MAX_VALUE)
-					.addComponent(btnOk))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblNombre)
+							.addPreferredGap(ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
+							.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE))
 					.addGap(68)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 139, GroupLayout.PREFERRED_SIZE)
-						.addComponent(chckbxPregunta1)
-						.addComponent(chckbxPregunta2))
-					.addContainerGap(71, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(chckbxPregunta2, GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(chckbxPregunta1)
+									.addPreferredGap(ComponentPlacement.RELATED))
+								.addComponent(rdbtnMujer)
+								.addComponent(rdbtnHombre)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+									.addComponent(btnLimpiar)
+									.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(6)
+									.addComponent(btnOk))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(btnOk_1)))))
+					.addGap(71))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNombre))
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNombre)
-								.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(18)
-							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
+							.addGap(36)
 							.addComponent(btnOk))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(20)
-							.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
 							.addGap(27)
-							.addComponent(chckbxPregunta1)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(chckbxPregunta2)))
-					.addContainerGap())
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(chckbxPregunta2)
+									.addGap(9)
+									.addComponent(chckbxPregunta1)
+									.addGap(11)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(rdbtnMujer)
+										.addComponent(btnOk_1))
+									.addGap(9)
+									.addComponent(rdbtnHombre)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addGap(18)
+									.addComponent(btnLimpiar)))))
+					.addGap(317))
 		);
 		
+		
 		txtArea = new JTextArea();
-		
-		
-		scrollPane.setViewportView(txtArea);
+		scrollPane.setColumnHeaderView(txtArea);
 		contentPane.setLayout(gl_contentPane);
 	}
 }
---
+++ InterfazGrafica/src/Interfaces/TablaPractica.java
@@ -0,0 +1,123 @@
+package Interfaces;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TablaPractica extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textClave;
+	private JTextField textValor;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TablaPractica frame = new TablaPractica();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TablaPractica() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblClave = new JLabel("Clave");
+		lblClave.setBounds(91, 153, 46, 14);
+		contentPane.add(lblClave);
+		
+		textClave = new JTextField();
+		textClave.setBounds(38, 173, 135, 20);
+		contentPane.add(textClave);
+		textClave.setColumns(10);
+		
+		JLabel lblValor = new JLabel("Valor");
+		lblValor.setBounds(284, 153, 46, 14);
+		contentPane.add(lblValor);
+		
+		textValor = new JTextField();
+		textValor.setBounds(231, 173, 135, 20);
+		contentPane.add(textValor);
+		textValor.setColumns(10);
+		
+		JButton btnAddd = new JButton("A\u00F1adir");
+		btnAddd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				textClave.setText("");
+				textValor.setText("");
+			}
+		});
+		btnAddd.setBounds(52, 227, 89, 23);
+		contentPane.add(btnAddd);
+		
+		JButton btnBorrar = new JButton("Borrar");
+		btnBorrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		btnBorrar.setBounds(176, 227, 89, 23);
+		contentPane.add(btnBorrar);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.setBounds(293, 227, 89, 23);
+		contentPane.add(btnModificar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(51, 21, 315, 122);
+		contentPane.add(scrollPane);
+		
+		
+		DefaultTableModel modelo= new DefaultTableModel();
+		scrollPane.setViewportView(table);
+		table = new JTable(modelo);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"Clave", "Valor",
+				
+				
+			}
+		));
+	}
+}
